7ab2bba16427fb8253bf56b931fb5597243a944f
yasminzuita <yasmin.zuita@student.reutlingen-university.de>
2025-11-29 21:35:47 +0100

    Refactor project structure: add ui, layout, and section folders; reorganize components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import Header from "@/app/components/Header";
-import Hero from "@/app/components/Hero";
-import About from "@/app/components/About";
-import Tutorials from "@/app/components/Tutorials";
-import Testimonials from "@/app/components/Testimonials";
-import Footer from "@/app/components/Footer";
+import Header from "@/app/components/layout/Header";
+import Hero from "@/app/components/sections/Hero/Hero";
+import About from "@/app/components/sections/About/About";
+import Tutorials from "@/app/components/sections/Tutorials/Tutorials";
+import Testimonials from "@/app/components/sections/Testimonials/Testimonials";
+import Footer from "@/app/components/layout/Footer";
 
 export default function Home() {
   return (
